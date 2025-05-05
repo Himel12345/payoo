fd1fcbc1d4076e728a784706dfcc3463f77a3b9a
Himel <you@example.com>
2025-05-06 03:14:49 +0600

    handle add money event handler on the button click

--- js/home.js
+++ js/home.js
@@ -1,1 +1,7 @@
-console.log('done')
+// console.log('done')
+document.getElementById('done').addEventListener('click', function(){
+    // console.log('clicked')
+    const addmoneyinput  = document.getElementById('add-money').value;
+    const pinnumberinput  = document.getElementById('pin-number').value;
+    console.log(addmoneyinput, pinnumberinput)
+})

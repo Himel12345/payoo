279428533cfc51dce9bb69e8b61ea44618e23f92
Himel <you@example.com>
2025-05-06 02:52:44 +0600

    created add money button and add money form

--- js/login.js
+++ js/login.js
@@ -1,9 +1,17 @@
-console.log("hello");
+// console.log("hello");
 document
   .getElementById("login-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    console.log("clicked");
+    // console.log("clicked");
     const phonenumber = document.getElementById("number").value;
-    console.log(phonenumber);
+    const pinnumber = document.getElementById("pin").value;
+    // console.log(phonenumber, pinnumber);
+    if (phonenumber === '01712912239' && pinnumber === '1969'){
+        console.log('you are logged in')
+        window.location.href = '/home.html'
+    }
+    else{
+        alert ('wrong')
+    }
   });

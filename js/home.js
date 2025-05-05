7b1e06e29079a1f3dfffdb02c0b1b662a07dadd1
Himel <you@example.com>
2025-05-06 03:36:26 +0600

    verify pin number and add money to the balance

--- js/home.js
+++ js/home.js
@@ -1,7 +1,21 @@
 // console.log('done')
-document.getElementById('done').addEventListener('click', function(){
-    // console.log('clicked')
-    const addmoneyinput  = document.getElementById('add-money').value;
-    const pinnumberinput  = document.getElementById('pin-number').value;
-    console.log(addmoneyinput, pinnumberinput)
-})
+document.getElementById("done").addEventListener("click", function () {
+  // console.log('clicked')
+  const addmoneyinput = document.getElementById("add-money").value;
+  const pinnumberinput = document.getElementById("pin-number").value;
+  console.log(addmoneyinput, pinnumberinput);
+  if (pinnumberinput === "1969") {
+   const balance = document.getElementById('current-balance').innerText;
+   console.log(typeof balance);
+   const addmoneynumber = parseFloat(addmoneyinput);
+   const balanceNumber = parseFloat(balance);
+   console.log(typeof balanceNumber)
+   const newbalance = addmoneynumber+ balanceNumber;
+   console.log(newbalance);
+
+    document.getElementById('current-balance').innerText = newbalance
+  } 
+  else {
+    alert("failed to add money");
+  }
+});
